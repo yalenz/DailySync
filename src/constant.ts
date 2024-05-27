0796b1fd6ee734ab812d77704bca34e205b66b77
yalenz <164312538+yalenz@users.noreply.github.com>
2024-05-27 18:39:41 +0800

    Update constant.ts

--- src/constant.ts
+++ src/constant.ts
@@ -10,16 +10,16 @@ export const AESKEY_DEFAULT = 'LSKDAJALSD';
  * GARMIN ACCOUNT
  */
 // 佳明中国区账号及密码
-export const GARMIN_USERNAME_DEFAULT = '';
-export const GARMIN_PASSWORD_DEFAULT = '';
+export const GARMIN_USERNAME_DEFAULT = 'yalenz@outlook.com';
+export const GARMIN_PASSWORD_DEFAULT = 'Yalen1121';
 // 佳明国际区区账号及密码
-export const GARMIN_GLOBAL_USERNAME_DEFAULT = '';
-export const GARMIN_GLOBAL_PASSWORD_DEFAULT = '';
+export const GARMIN_GLOBAL_USERNAME_DEFAULT = 'yalenz@outlook.com';
+export const GARMIN_GLOBAL_PASSWORD_DEFAULT = 'Yalen112';
 // 佳明迁移数量配置
-export const GARMIN_MIGRATE_NUM_DEFAULT = '';
-export const GARMIN_MIGRATE_START_DEFAULT = '';
+export const GARMIN_MIGRATE_NUM_DEFAULT = '100';
+export const GARMIN_MIGRATE_START_DEFAULT = '50';
 // 佳明每次同步时检查的最多的数量
-export const GARMIN_SYNC_NUM_DEFAULT = 10;
+export const GARMIN_SYNC_NUM_DEFAULT = 100;
 
 export const GARMIN_URL_DEFAULT = {
     'BASE_URL': 'https://connect.garmin.cn',
